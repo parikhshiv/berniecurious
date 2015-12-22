9efa438e417a0533232188695e3ed961d23bdae9
Shiv Parikh <shiv.parikh11@gmail.com>
2015-12-21 22:41:00 -0800

    restructured page

--- application.js
+++ application.js
@@ -1,42 +1,31 @@
 (function () {
-  var Video = window.Video = function () {
+  var Video = window.Video = function (infoTuple) {
     this.element = document.createElement("div");
-    this.element.className = "video";
+    this.element.className = "modal fade video";
+    this.element.setAttribute("id", infoTuple[1]);
+    this.topic = infoTuple[1];
+    this.header = infoTuple[0];
   }
 
   Video.prototype.create = function () {
-    this.addYoutube('SsDSbgEZswY');
-    addLinks.call(this);
+    this.addHeader();
+    this.addYoutube();
     return this.element;
   }
 
-  Video.prototype.addYoutube = function (topic) {
+  Video.prototype.addYoutube = function () {
     var youtube = document.createElement("embed")
-    youtube.setAttribute("src", "https://www.youtube.com/v/" + topic);
+    youtube.setAttribute("src", "https://www.youtube.com/v/" + this.topic);
     youtube.setAttribute("width", window.innerWidth / 2.5);
-    // youtube.setAttribute("height", 315);
 
     this.element.appendChild(youtube);
   }
 
-  var addLinks = function () {
-    var floatLeft = document.createElement("div")
-    var floatRight = document.createElement("div")
-    floatLeft.classList.add("left", "links");
-    floatRight.classList.add("right", "links");
-
-    var leftLink = document.createElement("a");
-    var rightLink = document.createElement("a");
-    leftLink.setAttribute("href", "http://berniesanders.com");
-    rightLink.setAttribute("href", "https://www.google.com/search?q=bernie+sanders&biw=1919&bih=935&noj=1&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjViNiRuebJAhULqh4KHYYvC_0Q_AUICCgB");
-    leftLink.textContent = "BernieSanders.com";
-    rightLink.textContent = "Media";
-
-    floatLeft.appendChild(leftLink);
-    floatRight.appendChild(rightLink);
-
-    this.element.appendChild(floatLeft);
-    this.element.appendChild(floatRight);
+  Video.prototype.addHeader = function () {
+    var header = document.createElement("h1");
+    header.classList.add("modal-header");
+    header.textContent = this.header;
+    this.element.appendChild(header);
   }
 }());
 
@@ -48,7 +37,7 @@
 
   var addHeader = function () {
     var header = document.createElement("h1");
-    header.textContent = "Curious About Bernie";
+    header.textContent = "Curious About Bernie?";
     this.element.appendChild(header);
   }
 
@@ -80,64 +69,72 @@
   var Footer = window.Footer = function () {
     this.element = document.createElement("div");
     this.element.classList.add("footer", "container-fluid");
-    this.videos = [["Wealth Inequality", 'Ui-fHDUnZDg'], ["Democratic Socialism", '6V1uBXMfTHY'],
-    ["Bernie's Solution", 'awXB5Ulaehk'], ["Poverty", 'wk0-eKo5rIk']];
+    this.videos = [["Wealth Inequality", 'Ui-fHDUnZDg'], ["Democratic Socialism", 'SsDSbgEZswY'],
+    ["The Solution", 'awXB5Ulaehk'], ["Poverty", 'wk0-eKo5rIk']];
   }
 
   Footer.prototype.create = function () {
+    addSubHeader.call(this, "See What He Has To Say About:");
     addSections.call(this, {"videos": this.videos});
     addLinks.call(this);
     return this.element;
   }
 
   var addSections = function (opts) {
-    var footerRow = document.createElement("div")
-    footerRow.classList.add("row");
+    var sectionsContainer = document.createElement("div");
+    sectionsContainer.classList.add("container-fluid");
+
     var num = (12 % opts.videos.length === 0) ? opts.videos.length : 3;
     for (var i = 0; i < num; i++) {
+      if (i % 2 == 0) {
+        var footerRow = document.createElement("div")
+        footerRow.classList.add("row");
+      }
       var label = opts.videos[i][0].split(" ")[0];
       this[label] = document.createElement("div");
-      this[label].classList.add("col-md-" + (12 / num), "footer-section");
+      this[label].classList.add("col-md-6", "footer-section");
       this[label].setAttribute("id", label);
 
-      var span = document.createElement("div")
-      span.textContent = opts.videos[i][0];
-
-      this[label].appendChild(span)
+      $(this[label]).text(opts.videos[i][0])
       footerRow.appendChild(this[label]);
+      if (i % 2 == 1) {
+        sectionsContainer.appendChild(footerRow);
+      }
     }
 
-    this.element.appendChild(footerRow);
+    sectionsContainer.appendChild(footerRow);
+    this.element.appendChild(sectionsContainer);
   }
 
-  var createPage = function (topic) {
-    var video = document.getElementsByClassName('video')[0]
-    var youtube = document.getElementsByTagName('embed')[0];
-    video.removeChild(youtube);
-    obj = {'element': video };
-
-    new window.Video().addYoutube.call(obj, topic);
+  var toggleModal = function (topic) {
+    $("#" + topic).modal('toggle');
   };
 
   var addLinks = function () {
     for (var i = 0; i < this.videos.length; i++) {
       var label = this.videos[i][0].split(" ")[0];
-      this[label].addEventListener('click', createPage.bind(null, this.videos[i][1]));
-    }
 
-    var header = document.getElementsByTagName('h1')[0];
-    header.addEventListener('click', createPage.bind(null, 'SsDSbgEZswY'));
+      $("#container").append(new window.Video(this.videos[i]).create());
+      this[label].addEventListener('click', toggleModal.bind(null, this.videos[i][1]));
+    }
+  };
+
+  var addSubHeader = function (text) {
+    $subHeader = $("<h3></h3>");
+    $subHeader.addClass("sub-head");
+    $subHeader.text(text);
+
+    $(this.element).append($subHeader);
   };
 }());
 
 (function() {
   var createMainPage = function () {
-    var app = document.getElementById('container');
-    app.textContent = "";
+    var $app = $('#container');
+    $app.text("");
 
-    app.appendChild(new window.Navbar().create());
-    app.appendChild(new window.Video().create());
-    app.appendChild(new window.Footer().create());
+    $app.append(new window.Navbar().create());
+    $app.append(new window.Footer().create());
   };
   createMainPage();
 }());
